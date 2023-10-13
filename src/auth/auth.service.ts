4c3aec0540d58653fa7651e310a4270976154945
Maxim Karachyov <karachyov.m@gmail.com>
2023-10-13 17:51:19 +0600

    auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
+import {UsersService} from "../users/users.service";
+import {JwtService} from "@nestjs/jwt";
+import * as bcrypt from "bcryptjs"
+import {User} from "../users/users.model";
+import {CreateUserDto} from "../users/dto/create-user.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private userService: UsersService,
+                private jwtService: JwtService) {
+    }
+
+    async login( userDto: CreateUserDto){
+        const user = await this.validateUser(userDto)
+        return this.generateTocken(user)
+
+    }
+
+
+    async registration( userDto: CreateUserDto){
+        const candidate = await this.userService.getUserByEmail(userDto.email)
+        if(candidate){
+            throw new HttpException('Пользователь с таким емайл существует ', HttpStatus.BAD_REQUEST)
+        }
+        const hashPassword = await bcrypt.hash(userDto.password, 5)
+        const user = await this.userService.createUser({...userDto, password: hashPassword})
+        return this.generateTocken(user)
+    }
+
+    private async generateTocken(user: User){
+        const payload = {email: user.email, id: user.id, role: user.roles}
+        return {
+            token: this.jwtService.sign(payload)
+        }
+    }
+
+    private async validateUser(userDto: CreateUserDto) {
+        const user = await this.userService.getUserByEmail(userDto.email)
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password)
+        if(user && passwordEquals){
+            return user
+        }
+        throw new UnauthorizedException({message:'Некорректная почта или пароль'})
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import {User} from "./users.model";
 import {InjectModel} from "@nestjs/sequelize";
 import {CreateUserDto} from "./dto/create-user.dto";
@@ -14,11 +14,17 @@ export class UsersService {
         const user = await this.userRepository.create(dto);
         const role = await this.RoleService.getRoleByValue('USER')
         await user.$set('roles', [role.id])
+        user.roles = role
         return user;
     }
 
     async getAllUsers() {
         const users = await this.userRepository.findAll({include: {all: true}});
         return users;
+    }
+
+    async getUserByEmail(email: string) {
+        const user = await this.userRepository.findOne({where: {email}, include: {all: true}})
+        return user
     }
 }
